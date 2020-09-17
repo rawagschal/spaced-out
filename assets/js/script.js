67c0e359899428e5e4294bf1c39657036018db3f
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-16 20:43:25 -0700

    Created a global variable for today's date, fetched mercuryretrograde API with today's date and console logged the response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 // Global Variable
-var date = 
+var currentDate = moment();
+console.log(currentDate)
 
-fetch ("https://mercuryretrogradeapi.com/?date=&{date}") {
-    
-}
+function getMercury() {
+    var date = moment(currentDate).format("(YYYY-MM-DD)")
+    fetch(
+        `https://mercuryretrogradeapi.com/?date=&${date}`
+    )
+    .then(function(mercuryResponse) {
+        return mercuryResponse.json();
+      })
+    .then(function(mercuryResponse) {
+        console.log(mercuryResponse.is_retrograde);
+    })
+};
+
+getMercury();
