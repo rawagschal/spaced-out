955ed61e751152edbdcd6e3b084fb8563023f187
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-16 20:56:49 -0700

    Added a button and display response of Mercury in retrograde

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Global Variable
 var currentDate = moment();
-console.log(currentDate)
+var mercuryRetroBtn = document.getElementById("button-1")
+
+$("#button-1").text("Is Mercury in Retrograde?");
+
+mercuryRetroBtn.onclick = getMercury;
 
 function getMercury() {
     var date = moment(currentDate).format("(YYYY-MM-DD)")
@@ -13,6 +17,13 @@ function getMercury() {
     .then(function(mercuryResponse) {
         console.log(mercuryResponse.is_retrograde);
     })
+    if ("mercuryResponse.is_retrograde" === "true") {
+        // Display the result
+        $("#button-1").append("<h1>True</h1>");
+    }
+    else {
+        // Display the result
+        $("#button-1").append("<h1>False</h1>");
+    }
 };
 
-getMercury();
