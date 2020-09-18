a3e6fd1ec3a1ea0516b5cd592d37740905c23cdc
Kim Mulligan <kimmariemulligan@gmail.com>
2020-09-18 15:13:35 -0700

    display Astros

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Global Variables
 var currentDate = moment();
-
-// Mercury Retrograde Button
+const astrosInSpace = []
+const astrosSection = document.getElementById('astroList')
+//Mercury Retrograde Button
 var mercuryRetroBtn = document.getElementById("button-1")
 $("#button-1").text("Is Mercury in Retrograde?");
 mercuryRetroBtn.onclick = getMercury;
@@ -29,40 +30,64 @@ function getMercury() {
 };
 
 console.log("hi");
-var test = function(){
-    fetch("http://api.open-notify.org/astros.json")
+
+var test = function () {
+  fetch("http://api.open-notify.org/astros.json")
     .then(function (response) {
-        return response.json()
+      return response.json();
     })
     .then(function (astroResponse) {
-        console.log(astroResponse);
-        // Array.forEach(element => {
-            
-        //  })
-        });  
-    fetch("https://spacelaunchnow.me/api/3.3.0/astronaut/?search=" + astroResponse.people[0].name.split(" ")[1])
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (bioResponse){
-        console.log(bioResponse);     
-    })
+      console.log(astroResponse);
+    astroResponse.people.forEach(element => {
+        fetch(
+            "https://spacelaunchnow.me/api/3.3.0/astronaut/?search=" +
+              element.name.split(" ")[1]
+          )
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (bioResponse) {
+              console.log(bioResponse);
+              astrosInSpace.push(bioResponse.results[0])
+    showAstros(bioResponse.results[0]);
+            });
+      })
+
+    });
 };
-
+function showAstros(astroObj){
+    console.log('finished loading' + astroObj.name);
+    astrosSection.innerHTML += `
+    <div class="row">
+                <div class="col s12 m12">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="${astroObj.profile_image}" />
+                      <span class="card-title">${astroObj.name}</span>
+                    </div>
+                    <div class="card-content">
+                      <p>
+                        ${astroObj.bio}
+                      </p>
+                    </div>
+                    <div class="card-action">
+                      <a href="${astroObj.wiki}">Wikipedia</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    `
+}
 test();
-    
-
 // Astrobin Image of the Day
 function getAstrobin() {
-    fetch(
-        `https://www.astrobin.com/api/v1/imageoftheday/?limit=1&api_key=44c950a81df45f010f9ada74487616c154e92b96&api_secret=9a1781a8d6378a530f3c81cf145464531279d658&format=json`
-    )
-    .then(function(response) {
-        return response.json();
-      })
-    .then(function(astrobinResponse) {
-        console.log(astrobinResponse.image);
+  fetch(
+    `https://www.astrobin.com/api/v1/imageoftheday/?limit=1&api_key=44c950a81df45f010f9ada74487616c154e92b96&api_secret=9a1781a8d6378a530f3c81cf145464531279d658&format=json`
+  )
+    .then(function (response) {
+      return response.json();
     })
-};
-
-    
+    .then(function (astrobinResponse) {
+      console.log(astrobinResponse.image);
+    });
+}
